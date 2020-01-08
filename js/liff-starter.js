4bada4585e5da2cdc3aa38bb93fa30cfb1d2aa11
ekadyah <eka.dyah1205@gmail.com>
2020-01-08 19:53:08 +0700

    first liff commit

--- js/liff-starter.js
+++ js/liff-starter.js
@@ -136,14 +136,18 @@ function btnHandlers() {
     
     document.getElementById('sendMessageButton').addEventListener('click', function() {
         if (liff.isInClient()) {
-            liff.sendMessages([{
-                'type': 'text',
-                'text': "You've successfully sent a message! Hooray!"
-            }]).then(function() {
-                window.alert('Message sent');
-            }).catch(function(error) {
-                window.alert('Error sending message: ' + error);
-            });
+            liff.sendMessages([
+                {
+                  type:'text',
+                  text:'Hello, World!'
+                }
+              ])
+              .then(() => {
+                console.log('message sent');
+              })
+              .catch((err) => {
+                console.log('error', err);
+              });
         } else {
             alert('ha');
         }
